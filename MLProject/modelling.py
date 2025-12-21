ba8969e8ea1a98c98db90f70b3cf2b259100930f
dewitaardn <ardiningsih.2305551045@student.unud.ac.id>
2025-12-21 22:27:40 +0800

    revisi

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,16 +1,8 @@
 import pandas as pd
-import dagshub
 import mlflow
 import mlflow.sklearn
-
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
-
-dagshub.init(
-    repo_owner="dewitaardn",
-    repo_name="Eksperimen_SML_Dewita",
-    mlflow=True
-)
 
 # Load dataset 
 DATA_DIR = "Membangun_model/heartDisease_preprocessing"
@@ -21,15 +13,13 @@ y_train = pd.read_csv(f"{DATA_DIR}/Y_train.csv").values.ravel()
 y_test  = pd.read_csv(f"{DATA_DIR}/Y_test.csv").values.ravel()
 
 # baseline model
-with mlflow.start_run(run_name="baseline_logreg"):
+with mlflow.start_run():
     model = LogisticRegression(max_iter=1000)
     model.fit(X_train, y_train)
 
     y_pred = model.predict(X_test)
     acc = accuracy_score(y_test, y_pred)
 
-    #Log ke MLflow
-    mlflow.log_param("model", "LogisticRegression")
     mlflow.log_metric("accuracy", acc)
     mlflow.sklearn.log_model(model, "model")
 
