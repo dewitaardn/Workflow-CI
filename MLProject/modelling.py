71edbd0930343cb4a19223e37aa3887e7feb5eb2
dewitaardn <ardiningsih.2305551045@student.unud.ac.id>
2025-12-22 00:07:46 +0800

    fixing modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,28 +1,46 @@
-import os
 import pandas as pd
 import mlflow
 import mlflow.sklearn
-from sklearn.linear_model import LogisticRegression
+import os
+
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DATA_DIR = os.path.join(BASE_DIR, "heartDisease_preprocessing")
+def load_data():
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    data_dir = os.path.join(base_dir, "heartDisease_preprocessing")
 
-# load data pakai os
-X_train = pd.read_csv(os.path.join(DATA_DIR, "X_train.csv"))
-X_test  = pd.read_csv(os.path.join(DATA_DIR, "X_test.csv"))
-y_train = pd.read_csv(os.path.join(DATA_DIR, "Y_train.csv")).values.ravel()
-y_test  = pd.read_csv(os.path.join(DATA_DIR, "Y_test.csv")).values.ravel()
+    X_train_path = os.path.join(data_dir, "X_train.csv")
+    X_test_path  = os.path.join(data_dir, "X_test.csv")
+    y_train_path = os.path.join(data_dir, "y_train.csv")
+    y_test_path  = os.path.join(data_dir, "y_test.csv")
 
-# baseline model
-with mlflow.start_run():
-    model = LogisticRegression(max_iter=1000)
-    model.fit(X_train, y_train)
+    for p in [X_train_path, X_test_path, y_train_path, y_test_path]:
+        if not os.path.exists(p):
+            raise FileNotFoundError(f"[ERROR] File tidak ditemukan: {p}")
 
-    y_pred = model.predict(X_test)
-    acc = accuracy_score(y_test, y_pred)
+    X_train = pd.read_csv(X_train_path)
+    X_test  = pd.read_csv(X_test_path)
+    y_train = pd.read_csv(y_train_path).values.ravel()
+    y_test  = pd.read_csv(y_test_path).values.ravel()
 
-    mlflow.log_metric("accuracy", acc)
-    mlflow.sklearn.log_model(model, "model")
+    return X_train, y_train, X_test, y_test
 
-    print("Accuracy:", acc)
+
+def train_basic_model(X_train, y_train, X_test, y_test):
+    mlflow.autolog()
+
+    with mlflow.start_run():
+        model = RandomForestClassifier(
+            n_estimators=100,
+            random_state=42,
+            n_jobs=-1
+        )
+        model.fit(X_train, y_train)
+
+        y_pred = model.predict(X_test)
+        acc = accuracy_score(y_test, y_pred)
+
+if __name__ == "__main__":
+    X_train, y_train, X_test, y_test = load_data()
+    train_basic_model(X_train, y_train, X_test, y_test)
